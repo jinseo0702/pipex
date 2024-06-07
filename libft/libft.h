8bdab5bca03b7905644ec0dd7cd941c78d6d5d06
Jinkwon Seo <jinseo@c2r18s1.42gyeongsan.kr>
2024-06-07 10:55:38 +0900

    add_ft_free_split is free two dimensional array that use in split

--- libft/libft.h
+++ libft/libft.h
@@ -38,6 +38,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void    ft_free_split(char **split);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
